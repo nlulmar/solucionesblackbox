feafa705906cec0035f43fb7fa97640397a3e827
Nitin Kumar <nlulmar@gmail.com>
2022-07-25 09:42:31 +0100

    solución blackbox 1

---
+++ solucionesblackbox1.scala
@@ -0,0 +1,6 @@
+def filtroPorAnyo (secuencia: List[String], anyo: Int): List[String] = secuencia.filter(_.take(4) == anyo.toString)
+
+val secuencia = List("2020-1-4", "2019-4-7", "2021-12-11", "2020-8-9")
+val anyo = 2020
+
+println(filtroPorAnyo(secuencia,anyo))
